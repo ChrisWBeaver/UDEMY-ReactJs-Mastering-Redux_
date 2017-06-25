7a58c704af84aec02a341614c5b7870f6fd139f7
Chris Beaver <chrisbvr@gmail.com>
2017-06-25 17:14:23 +1000

    added fade in

--- ageteller/src/components/App.js
+++ ageteller/src/components/App.js
@@ -40,7 +40,9 @@ class App extends Component {
                     </Button>
                     {
                         this.state.showStats ? 
-                        <AgeStats date={this.state.birthday} />
+                        <div className="fade age-stats">
+                          <AgeStats date={this.state.birthday} />
+                        </div>
                         :
                         <div></div>
                     }
