2eb127af21edd8c0b114aab23e6037108b5e7eb0
Chris Beaver <chrisbvr@gmail.com>
2017-06-25 17:02:21 +1000

    year calculation (kinda) :D

--- ageteller/src/components/AgeStats.js
+++ ageteller/src/components/AgeStats.js
@@ -6,8 +6,16 @@ class AgeStats extends Component {
     let today = new Date().getTime(); 
     let other_date = new Date(date).getTime(); 
     let difference = Math.abs(today - other_date); 
+    
+    let days = Math.floor(difference / (1000 * 3600 * 24)); 
+    
+    let years = Math.floor(days / 365); 
+    days -= years * 365; 
 
-    return `${difference} milliseconds`
+    let months = Math.floor(days/31); 
+    days-= months * 31; 
+
+    return `${years} years, ${months} months, and ${days} days`
 
   }
   render() {
