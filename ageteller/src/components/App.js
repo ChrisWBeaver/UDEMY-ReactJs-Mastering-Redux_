1244fc99e90c2d450713f78e9928d954d81c15c8
Chris Beaver <chrisbvr@gmail.com>
2017-06-25 15:58:11 +1000

    basic component

---
+++ ageteller/src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+      return (
+          <div>ageteller component</div>
+      )
+  }    
+}
+
+export default App; 
--- ageteller/src/index.js
+++ ageteller/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
 
 ReactDOM.render(
-     <div> ageteller </div>, document.getElementById('root')
+     <App></App>, document.getElementById('root')
 )
