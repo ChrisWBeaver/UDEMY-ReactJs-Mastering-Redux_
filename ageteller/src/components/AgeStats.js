a7020e5bcea3f8d090baef76b3489c820bd4ced6
Chris Beaver <chrisbvr@gmail.com>
2017-06-25 16:59:15 +1000

    added timesince function

--- ageteller/src/components/AgeStats.js
+++ ageteller/src/components/AgeStats.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'; 
 
 class AgeStats extends Component {
+
+  timeSince(date){
+    let today = new Date().getTime(); 
+    let other_date = new Date(date).getTime(); 
+    let difference = Math.abs(today - other_date); 
+
+    return `${difference} milliseconds`
+
+  }
   render() {
     return (
       <div>
         <h3>{this.props.date}</h3>
+        <h4>Congrats {this.timeSince(this.props.date)}!</h4>
       </div>
 
     )
