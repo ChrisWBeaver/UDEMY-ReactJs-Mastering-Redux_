6b253c639e9169199958e99151340d4ff82831cb
Chris Beaver <chrisbvr@gmail.com>
2017-06-26 19:57:27 +1000

    added hero reducer

--- supersquad/src/index.js
+++ supersquad/src/index.js
@@ -7,9 +7,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
+import { addCharacterById } from './actions'; 
 
 const store = createStore(rootReducer); 
+store.subscribe(() => console.log('store', store.getState())); 
 
+store.dispatch(addCharacterById(2)); 
 
 ReactDOM.render(
   <Provider store={store}>
--- supersquad/src/reducers/index.js
+++ supersquad/src/reducers/index.js
@@ -7,12 +7,27 @@ function characters(state = characters_json, action){
     case ADD_CHARACTER: 
       // return all characters that are not the one in the action 
       // because we are removing it :D 
-      let characters = state.filter(item => item.id != action.id);
+      let characters = state.filter(item => item.id !== action.id);
       return characters; 
     default:
       return state; 
       
   }
+}
+
+function heros(state = [], action){
+  switch (action.type){
+    case ADD_CHARACTER:
+      let heros = [...state, createCharacter(action.id)];
+      return heros;  
+    default: 
+      return state; 
+  }
+}
+
+
+function createCharacter(id){
+  let characters = characters_json.find( c => c.id === id);
 }
 
 export default characters; 
