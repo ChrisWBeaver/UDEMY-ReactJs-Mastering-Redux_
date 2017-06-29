ab0e975a442c1f114525fd5335582c0978b34590
Chris Beaver <chrisbvr@gmail.com>
2017-06-29 19:20:35 +1000

    added add character by id to an onclick element

--- supersquad/src/components/CharacterList.js
+++ supersquad/src/components/CharacterList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux'; //connects to the state lol
+import { bindActionCreators } from 'redux';
+import { addCharacterById } from '../actions';
 
 class CharacterList extends Component {
   render() {
@@ -13,6 +15,9 @@ class CharacterList extends Component {
               return (
                 <li key={character.id}>
                   <div>{character.name}</div>
+                  <div onClick={() => this.props.addCharacterById(character.id)}>
+                    +
+                  </div>
                 </li>
               )
             })
@@ -32,5 +37,11 @@ function mapStateToProps(state) {
   }; 
 }
 
+function mapDispatchtoProps(dispatch){
+  //bind the action creaters to the props of this component
+  return bindActionCreators({ addCharacterById}, dispatch);
+}
+
 //connect character list to redux
-export default connect(mapStateToProps, null)(CharacterList);   
+// map the dispatch to props
+export default connect(mapStateToProps, mapDispatchtoProps)(CharacterList);   
