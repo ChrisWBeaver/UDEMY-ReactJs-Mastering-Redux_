527e0926200e62d955652811b0910dc3b4872471
Chris Beaver <chrisbvr@gmail.com>
2017-06-29 20:06:46 +1000

    added squad stats

--- supersquad/src/components/App.js
+++ supersquad/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import CharacterList from './CharacterList'; 
 import HeroList from './HeroList';
+import SquadStats from './SquadStats';
 import '../style/index.css';
 
 class App extends Component {
@@ -9,11 +10,14 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Super Squad</h2>
-        <div className="col-md-6">
+        <div className="col-md-4">
           <CharacterList />
         </div>
-        <div className="col-md-6">
+        <div className="col-md-4">
           <HeroList />
+        </div>
+        <div className="col-md-4">
+          <SquadStats />
         </div>
       </div>
 
