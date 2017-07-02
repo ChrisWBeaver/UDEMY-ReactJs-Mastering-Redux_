33b01061412c918ea38ae3b9fefa5543fb37116f
Chris Beaver <chrisbvr@gmail.com>
2017-07-02 20:02:57 +1000

    adding recipes reducer

---
+++ recipefinder/src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+import { SET_RECIPES } from '../actions';
+
+function recipes(state = [], action){
+  switch(action.type){
+    case SET_RECIPES: 
+      return action.items;
+    default: 
+      return state; 
+  }
+}
+
+const rootReducer = combineReducers({ recipes })
