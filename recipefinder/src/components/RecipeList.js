415a466be0a3dfcbe9f0b8bb2987b93496402555
Chris Beaver <chrisbvr@gmail.com>
2017-07-02 20:10:45 +1000

    added initial recipe list component

---
+++ recipefinder/src/components/RecipeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+import { connect } from 'react-redux'; 
+
+class RecipeList extends Component {
+  render(){
+    console.log('this.props', this.props);
+
+    return (
+      <div>
+      </div>
+    )
+  }
+}
