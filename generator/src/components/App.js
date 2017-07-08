d36b864a25bf5adafd92126b5397e5703665736f
Chris Beaver <chrisbvr@gmail.com>
2017-07-08 20:05:24 +1000

    added 10 meme limit

--- generator/src/components/App.js
+++ generator/src/components/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
-
+  constructor() {
+    super(); 
+    this.state = {
+      memeLimit:10
+    }
+  }
   render(){
     return(
       <div>
         <h2>Welcome to the Meme Generator</h2>
         {
-          this.props.memes.map((meme, index) => {
+          this.props.memes
+          .slice(0, this.state.memeLimit)
+          .map((meme, index) => {
             return (
               <h4 key={index}>{meme.name}</h4>
             )
           })
         }
+        <div onClick={() => 
+                           {
+                             this.setState({memeLimit: this.state.memeLimit + 10})
+                           }
+                     }>Load 10 more memes...
+        </div>
       </div>
     )
   }
