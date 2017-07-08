03afa9bd838819818260bfb47d181179a2778666
Chris Beaver <chrisbvr@gmail.com>
2017-07-08 16:54:08 +1000

    initial setup of memegenerator

---
+++ generator/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class App extends Component {
+
+  render(){
+    return(
+      <div>
+        <h2>Welcome to the Meme Generator</h2>
+      </div>
+    )
+  }
+}
+
+export default App; 
