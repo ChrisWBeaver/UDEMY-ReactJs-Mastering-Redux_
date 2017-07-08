192b9df73f59dfaccd12e32aa8af96daf75ae8c5
Chris Beaver <chrisbvr@gmail.com>
2017-07-08 20:32:02 +1000

    added animations

--- generator/src/components/MemeItem.js
+++ generator/src/components/MemeItem.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'; 
 
 class MemeItem extends Component {
+  constructor() {
+    super(); 
+
+    this.state = {
+      hovered: false
+    }
+  }
+
   render(){
     return (
-      <div className="meme-item">
+      <div 
+        className="meme-item"
+        onMouseEnter={() => this.setState({hovered: true})}
+        onMouseLeave={() => this.setState({hovered: false})}>
         <img
           src={this.props.meme.url}
           alt={this.props.meme.name}
-          className="meme-img"
+          className={this.state.hovered ? "meme-img darken-img" : "meme-img"}
         />
-        <p className="meme-text">
+        <p className={this.state.hovered ? "meme-text" : "no-text"}>
           {this.props.meme.name}
         </p>
       </div>
