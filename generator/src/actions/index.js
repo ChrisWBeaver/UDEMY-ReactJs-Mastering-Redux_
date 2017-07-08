1a75f8da9bc0366d667f8566c5272fa7172b421e
Chris Beaver <chrisbvr@gmail.com>
2017-07-08 19:21:48 +1000

    added receiveMames action creator

---
+++ generator/src/actions/index.js
@@ -0,0 +1,23 @@
+export const RECEIVE_MEMES = 'RECEIVE_MEMES';
+
+
+function receiveMemes(json){
+  const {memes} = json.data; 
+  
+  return {
+    type: RECEIVE_MEMES, 
+    memes
+  }
+}
+
+function fetchMemesJson(){
+  return fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+}
+
+export function fetchMemes() {
+  return function(dispatch){
+    return fetchMemesJson()
+    .then(json => dispatch(receiveMemes(json)));
+  }
+}
